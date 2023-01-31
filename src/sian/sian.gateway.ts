646c5049294bb1f251a9462f8e71d41b01a37db6
caiovsoares <caio.soares@sou.fae.br>
2023-01-31 18:15:59 -0300

    ping

--- src/sian/sian.gateway.ts
+++ src/sian/sian.gateway.ts
@@ -2,12 +2,15 @@ import {
   ConnectedSocket,
   SubscribeMessage,
   WebSocketGateway,
+  OnGatewayConnection,
 } from '@nestjs/websockets';
 import { MessageBody } from '@nestjs/websockets/decorators';
 import { Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class SianGateway {
+export class SianGateway implements OnGatewayConnection {
+  players = [];
+
   @SubscribeMessage('message')
   async handleMessage(
     @MessageBody() data: string,
@@ -19,8 +22,12 @@ export class SianGateway {
   }
 
   @SubscribeMessage('ping')
-  ping(@ConnectedSocket() client: Socket) {
-    console.log('ping');
+  ping(@ConnectedSocket() client: Socket, @MessageBody() data: string) {
     client.emit('pong', 'pong');
+  }
+
+  handleConnection(@ConnectedSocket() client: Socket, ...args: any[]) {
+    this.players.push({ x: 1, y: 2, z: 3 });
+    console.log(`Servidor recebeu conexão com ID: ${client.id}`);
   }
 }
